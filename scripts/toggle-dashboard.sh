71a10c43e898280e230118c6bf8f6ca00eade6db
Andrej235 <andrej.nenadic06@gmail.com>
2025-01-18 16:42:41 +0100

    Create an overlay window which covers the entire screen with a half-transparent box and renders the dashboard on top of it

--- scripts/toggle-dashboard.sh
+++ scripts/toggle-dashboard.sh
@@ -1,6 +1,10 @@
 current=$(eww state | grep 'is_dashboard_active' | awk '{print $2}')
 if [ "$current" = "true" ]; then
     eww update is_dashboard_active="false"
+    sleep 0.5s
+    eww close overlay
 else
+    eww open overlay
+    sleep 0.05s
     eww update is_dashboard_active="true"
 fi
